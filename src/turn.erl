f425e928fd5303b9f056326d3a9d49e089c31257
Christoph Pech <christoph.pech@gmail.com>
2018-08-24 12:14:30 +0200

    order reversed

--- src/turn.erl
+++ src/turn.erl
@@ -334,10 +334,6 @@ handle_info({udp, Sock, Addr, Port, Data}, StateName, State) ->
     inet:setopts(Sock, [{active, once}]),
 	Channel = find_channel(Addr, Port, State),
     case Channel of
-		_ -> 
-%%			?dbg("handle_info indication ok c, ~s",[addr_to_str({Addr, Port})]),
-			TurnMsg = #turn{channel = Channel, data = Data},
-			{next_state, StateName, send(State, TurnMsg)};
 		undefined ->
 %%			?dbg("handle_info indication ok u, ~s",[addr_to_str({Addr, Port})]),
 			Seq = State#state.seq,
@@ -346,7 +342,11 @@ handle_info({udp, Sock, Addr, Port, Data}, StateName, State) ->
 				trid = Seq,
 				'XOR-PEER-ADDRESS' = [{Addr, Port}],
 				'DATA' = Data},
-			{next_state, StateName, send(State#state{seq = Seq+1}, Ind)}
+			{next_state, StateName, send(State#state{seq = Seq+1}, Ind)};
+		_ -> 
+%%			?dbg("handle_info indication ok c, ~s",[addr_to_str({Addr, Port})]),
+			TurnMsg = #turn{channel = Channel, data = Data},
+			{next_state, StateName, send(State, TurnMsg)}
     end;
 handle_info({timeout, _Tref, stop}, _StateName, State) ->
     {stop, normal, State};
