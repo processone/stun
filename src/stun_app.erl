acadaf9e1aea3cccbac51c4aa41cf0420c89fabe
Badlop <badlop@process-one.net>
2022-02-02 12:39:52 +0100

    Remove generic spec comments

--- src/stun_app.erl
+++ src/stun_app.erl
@@ -32,22 +32,6 @@
 %%% Application callbacks
 %%%===================================================================
 
-%%--------------------------------------------------------------------
-%% @private
-%% @doc
-%% This function is called whenever an application is started using
-%% application:start/[1,2], and should start the processes of the
-%% application. If the application is structured according to the OTP
-%% design principles as a supervision tree, this means starting the
-%% top supervisor of the tree.
-%%
-%% @spec start(StartType, StartArgs) -> {ok, Pid} |
-%%                                      {ok, Pid, State} |
-%%                                      {error, Reason}
-%%      StartType = normal | {takeover, Node} | {failover, Node}
-%%      StartArgs = term()
-%% @end
-%%--------------------------------------------------------------------
 start(_StartType, _StartArgs) ->
     stun_logger:start(),
     application:start(fast_tls),
@@ -58,16 +42,6 @@ start(_StartType, _StartArgs) ->
             Error
     end.
 
-%%--------------------------------------------------------------------
-%% @private
-%% @doc
-%% This function is called whenever an application has stopped. It
-%% is intended to be the opposite of Module:start/2 and should do
-%% any necessary cleaning up. The return value is ignored.
-%%
-%% @spec stop(State) -> void()
-%% @end
-%%--------------------------------------------------------------------
 stop(_State) ->
     stun_logger:stop(),
     ok.
--- src/turn_tmp_sup.erl
+++ src/turn_tmp_sup.erl
@@ -37,13 +37,6 @@
 %%% API functions
 %%%===================================================================
 
-%%--------------------------------------------------------------------
-%% @doc
-%% Starts the supervisor
-%%
-%% @spec start_link() -> {ok, Pid} | ignore | {error, Error}
-%% @end
-%%--------------------------------------------------------------------
 start_link() ->
     supervisor:start_link({local, ?SERVER}, ?MODULE, []).
 
@@ -51,19 +44,6 @@ start_link() ->
 %%% Supervisor callbacks
 %%%===================================================================
 
-%%--------------------------------------------------------------------
-%% @private
-%% @doc
-%% Whenever a supervisor is started using supervisor:start_link/[2,3],
-%% this function is called by the new process to find out about
-%% restart strategy, maximum restart frequency and child
-%% specifications.
-%%
-%% @spec init(Args) -> {ok, {SupFlags, [ChildSpec]}} |
-%%                     ignore |
-%%                     {error, Reason}
-%% @end
-%%--------------------------------------------------------------------
 init([]) ->
     {ok, {{simple_one_for_one, 10, 1},
           [{undefined, {turn, start_link, []},
